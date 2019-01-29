a78bb7527c11aafdb0bdc854324214210a7992ca
Darina Kavokina <kuz-dana@mail.ru>
2019-01-29 21:32:38 +0300

    Issue 2.0: Prepare classes for cards

---
+++ Server/CardManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLibrary;
+
+namespace Server
+{
+	class CardManager
+	{
+		public Queue<Card> SuffleCards(List<Card> InitialSet)
+		{
+			int setLength = InitialSet.Count;
+
+			return null;
+		}
+	}
+}
