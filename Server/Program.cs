0f78f1f43ecddd7446dfcf12d1724fd4e92f7bf6
alexanderkavokin <alexanderkavokin@gmail.com>
2019-01-29 22:07:32 +0100

    added server logic with TcpListener

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CommonLibrary.Tcp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,47 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse(TcpConfig.Ip);
+                server = new TcpListener(localAddr, TcpConfig.Port);
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connections...");
+
+                    // получаем входящее подключение
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+                    // получаем сетевой поток для чтения и записи
+                    NetworkStream stream = client.GetStream();
+
+                    // сообщение для отправки клиенту
+                    string response = "Привет мир";
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Отправлено сообщение: {0}", response);
+                    // закрываем поток
+                    stream.Close();
+                    // закрываем подключение
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
         }
     }
 }
