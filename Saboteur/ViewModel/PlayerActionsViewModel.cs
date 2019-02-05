3dd1f9b85c323bede282e3d63d3ff2662bf83a9b
alexanderkavokin <alexanderkavokin@gmail.com>
2019-02-05 18:31:04 +0100

    stage commit

--- Saboteur/ViewModel/PlayerActionsViewModel.cs
+++ Saboteur/ViewModel/PlayerActionsViewModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using CommonLibrary.Tcp;
 using Saboteur.Annotations;
 using Saboteur.MVVM;
 
@@ -14,6 +16,26 @@ namespace Saboteur.ViewModel
 {
     public class PlayerActionsViewModel : ViewModelBase
     {
+        #region Fields
+
+        private readonly TcpClient _client;
+        private NetworkStream _stream;
+
+        #endregion
+
+        #region Constructors
+
+        public PlayerActionsViewModel()
+        {
+            _client = new TcpClient();
+            _client.Connect(TcpConfig.Ip, TcpConfig.Port);
+            _stream = _client.GetStream();
+        }
+
+        #endregion
+
+        #region Commands
+
         private RelayCommand _buildCommand;
 
         public ICommand BuildCommand => _buildCommand ?? (_buildCommand =
@@ -21,12 +43,16 @@ namespace Saboteur.ViewModel
 
         private void ExecuteBuildCommand(object obj)
         {
-            MessageBox.Show("ololo");
+            string response = "Build!";
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            _stream.Write(data, 0, data.Length);
         }
 
         private bool CanExecuteBuildCommand(object obj)
         {
             return true;
         }
+
+        #endregion
     }
 }
