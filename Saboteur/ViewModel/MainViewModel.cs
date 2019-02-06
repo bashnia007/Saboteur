5c3a8d2a2ff9b88dab6d0438119f94e8e1050de4
alexanderkavokin <alexanderkavokin@gmail.com>
2019-02-06 22:56:35 +0100

    changed name for login window, added player hand user control, added main window with 2 hands, added viewmodels for all views above

---
+++ Saboteur/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Saboteur.MVVM;
+
+namespace Saboteur.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public readonly MainWindow Window;
+
+        public MainViewModel()
+        {
+            Window = new MainWindow();
+            Window.DataContext = this;
+        }
+    }
+}
---
+++ Saboteur/ViewModel/PlayerHandViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLibrary.CardsClasses;
+using Saboteur.MVVM;
+
+namespace Saboteur.ViewModel
+{
+    public class PlayerHandViewModel : ViewModelBase
+    {
+        public ObservableCollection<HandCard> Cards { get; set; }
+
+        public PlayerHandViewModel()
+        {
+            Cards = new ObservableCollection<HandCard>();
+            Cards.Add(new HandCard());
+        }
+    }
+}
