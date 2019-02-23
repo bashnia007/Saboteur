56b2d2a8e554e0d9437497f80ea1324a1d5ed2da
Darina Kavokina <kuz-dana@mail.ru>
2019-02-24 00:42:03 +0300

    Issue 14.4: Added implementation to ValidateBuildingTunnelAction method

--- CommonLibrary/CardsClasses/RouteCard.cs
+++ CommonLibrary/CardsClasses/RouteCard.cs
@@ -11,5 +11,25 @@ namespace CommonLibrary.CardsClasses
 		public RouteCard(int id, string imagePath) : base(id, imagePath) { }
 
         public RouteCard(int id) : base(id) { }
+
+		#region Параметры возможности присоединения к карточке
+		public bool TopJoining { get; set; }
+		public bool BottomJoining { get; set; }
+		public bool RightJoining { get; set; }
+		public bool LeftJoining { get; set; }
+		#endregion
+
+		#region Параметры проходимости туннеля
+		public bool PassableThough { get; set; }
+		public bool PassableThoughHorisontal { get; set; }
+		public bool PassableTroughVertical { get; set; }
+		public bool PassableBlueOnly { get; set; }
+		public bool PassableGreenOnly { get; set; }
+		public bool PassableRight2Top { get; set; }
+		public bool PassableRight2Bottom { get; set; }
+		public bool PassableLeft2Top { get; set; }
+		public bool PassableLeft2Bottom { get; set; }
+		public bool NonPassable { get; set; }
+		#endregion
 	}
 }
--- Server/Validator.cs
+++ Server/Validator.cs
@@ -14,7 +14,52 @@ namespace Server
 
 		public bool ValidateBuildingTunnelAction(RouteCard routeCard, List<RouteCard> tableRouteCards)
 		{
+			int x = routeCard.Coordinates.Coordinate_X;
+			int y = routeCard.Coordinates.Coordinate_Y;
 
+			bool a, b, c, d;
+
+			// Надо переписать так, чтобы булево значение карты с координатой x было равно булеву значению  карты x + 1.
+			// И аналогично для других, но фиг знает как это сделать =(((((
+			if (routeCard.TopJoining == true)
+			{
+				if (y == y + 1 && routeCard.BottomJoining == true)
+				{
+					return a = true;
+				}
+				else return a = false;
+			}
+			if (routeCard.BottomJoining == true)
+			{
+				if (y == y - 1 && routeCard.TopJoining == true)
+				{
+					return b = true;
+				}
+				else return b = false;
+			}
+			if (routeCard.RightJoining == true)
+			{
+				if (x == x + 1 && routeCard.LeftJoining == true)
+				{
+					return c = true;
+				}
+				else return c = false;
+			}
+			if (routeCard.LeftJoining == true)
+			{
+				if (x == x - 1 && routeCard.RightJoining == true)
+				{
+					return d = true;
+				}
+				else return d = false;
+			}
+			
+			//ValidateCanPassTunnel();
+			throw new NotImplementedException();
+		}
+
+		public bool ValidateCanPassTunnel(RouteCard routeCard, List<RouteCard> tableRouteCards)
+		{
 			throw new NotImplementedException();
 		}
 	}
