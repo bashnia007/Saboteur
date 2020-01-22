5ac14f61c9a36f29867e31ce188a12a2ece63af3
alexanderkavokin <alexanderkavokin@gmail.com>
2020-01-22 23:25:39 +0100

    added equipment control

--- XamarinApp/XamarinApp/Views/Controls/PlayerEquipment.xaml.cs
+++ XamarinApp/XamarinApp/Views/Controls/PlayerEquipment.xaml.cs
@@ -9,12 +9,74 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinApp.Views.Controls
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PlayerEquipment : ContentView
+	public partial class PlayerEquipment : Grid
 	{
 		public PlayerEquipment ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        public static readonly BindableProperty LampProperty = 
+            BindableProperty.Create(nameof(Lamp), typeof(string), typeof(PlayerEquipment), default(string), BindingMode.TwoWay);
+        public string Lamp
+        {
+            get
+            {
+                return (string)GetValue(LampProperty);
+            }
+
+            set
+            {
+                SetValue(LampProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty PickProperty =
+            BindableProperty.Create(nameof(Pick), typeof(string), typeof(PlayerEquipment), default(string), BindingMode.TwoWay);
+        public string Pick
+        {
+            get
+            {
+                return (string)GetValue(PickProperty);
+            }
+
+            set
+            {
+                SetValue(PickProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty TrolleyProperty =
+            BindableProperty.Create(nameof(Trolley), typeof(string), typeof(PlayerEquipment), default(string), BindingMode.TwoWay);
+        public string Trolley
+        {
+            get
+            {
+                return (string)GetValue(TrolleyProperty);
+            }
+
+            set
+            {
+                SetValue(TrolleyProperty, value);
+            }
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == LampProperty.PropertyName)
+            {
+                LampImage.Source = Lamp;
+            }
+            else if (propertyName == PickProperty.PropertyName)
+            {
+                PickImage.Source = Pick;
+            }
+            else if (propertyName == TrolleyProperty.PropertyName)
+            {
+                TrolleyImage.Source = Trolley;
+            }
+        }
+    }
 }
